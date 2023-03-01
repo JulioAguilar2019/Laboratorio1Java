c8e8c444535bab63703c5196c952cefc5eb51a3c
Julio Aguilar <julioa.m777@hotmail.com>
2023-03-01 13:00:43 -0600

    Class connection

---
+++ src/main/java/DB/Connect.java
@@ -0,0 +1,23 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connect {
+
+    Connection connection;
+    String url = "jdbc:mysql://localhost:3306/laboratorio1";
+    String user = "root";
+    String password = "";
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return connection;
+    }
+
+}
