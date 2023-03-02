dd9fd47bb26d8837a78d40df3ca2d9d1b12dea1f
Mario Ernesto Carranza <mario.carranza996@gmail.com>
2023-03-01 19:48:27 -0600

    Crud clientes

--- src/main/java/com/example/laboratorio1/Controller/ClienteController.java
+++ src/main/java/com/example/laboratorio1/Controller/ClienteController.java
@@ -10,18 +10,17 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet(name = "ClienteController", value = "/Cliente")
 public class ClienteController extends HttpServlet {
+    DB.Connect cn = new DB.Connect();
+    ClienteDAO dao = new ClienteDAO(cn.getConnection());
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
-        DB.Connect cn = new DB.Connect();
-
-        ClienteDAO dao = new ClienteDAO(cn.getConnection());
 
         try {
             String json = new Gson().toJson(dao.listar());
@@ -63,16 +62,64 @@ public class ClienteController extends HttpServlet {
         }
     }
 
-    private void deleteClient(HttpServletRequest request, HttpServletResponse response) {
+    private void deleteClient(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JsonObject data = new Gson().fromJson(request.getReader(), JsonObject.class);
+        int id = data.get("id").getAsInt();
+
+        PrintWriter out = response.getWriter();
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        Map jsonRespo = new HashMap<>();
+        String json = "";
+
+        try {
+            dao.eliminar(id);
+            jsonRespo.put("message", "Cliente eliminado");
+            jsonRespo.put("status", "200");
+            json = new Gson().toJson( jsonRespo );
+
+            out.print(json);
+            out.flush();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            jsonRespo.put("message", "Error al eliminar cliente");
+            jsonRespo.put("status", "500");
+            json = new Gson().toJson( jsonRespo );
+
+            out.print(json);
+            out.flush();
+        }
     }
 
-    private void updateClient(HttpServletRequest request, HttpServletResponse response) {
+    private void updateClient(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JsonObject data = new Gson().fromJson(request.getReader(), JsonObject.class);
+        int id = data.get("id").getAsInt();
+
+        PrintWriter out = response.getWriter();
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        Map jsonRespo = new HashMap<>();
+        String json = "";
+
+        try {
+            Cliente cliente = dao.buscarPorId(id);
+            jsonRespo.put("message", "Cliente encontrado");
+            jsonRespo.put("status", "200");
+            jsonRespo.put("data", cliente);
+            json = new Gson().toJson( jsonRespo );
+
+            out.print(json);
+            out.flush();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            jsonRespo.put("message", "Error al buscar cliente");
+            jsonRespo.put("status", "400");
+            json = new Gson().toJson( jsonRespo );
+
+            out.print(json);
+            out.flush();
+        }
     }
 
     private void saveClient(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        DB.Connect cn = new DB.Connect();
-        ClienteDAO dao = new ClienteDAO(cn.getConnection());
-
         JsonObject data = new Gson().fromJson(request.getReader(), JsonObject.class);
         String nombre = data.get("nombre").getAsString();
         String direccion = data.get("direccion").getAsString();
@@ -105,6 +152,44 @@ public class ClienteController extends HttpServlet {
 
     }
 
-    private void editClient(HttpServletRequest request, HttpServletResponse response) {
+    private void editClient(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException {
+        JsonObject data = new Gson().fromJson(request.getReader(), JsonObject.class);
+
+        int id = data.get("id").getAsInt();
+        String nombre = data.get("nombre").getAsString();
+        String direccion = data.get("direccion").getAsString();
+        String telefono = data.get("telefono").getAsString();
+        String email = data.get("email").getAsString();
+
+        PrintWriter out = response.getWriter();
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        Map jsonRespo = new HashMap<>();
+        String json = "";
+
+        Cliente cliente = dao.buscarPorId(id);
+
+        cliente.setNombre(nombre);
+        cliente.setDireccion(direccion);
+        cliente.setTelefono(telefono);
+        cliente.setEmail(email);
+
+        try {
+            dao.actualizar( cliente );
+            jsonRespo.put("message", "Cliente actualizado");
+            jsonRespo.put("status", "200");
+            jsonRespo.put("data", cliente);
+            json = new Gson().toJson( jsonRespo );
+
+            out.print(json);
+            out.flush();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            jsonRespo.put("message", "Error al actualizar cliente");
+            jsonRespo.put("status", "500");
+            json = new Gson().toJson( jsonRespo );
+
+            out.print(json);
+            out.flush();
+        }
     }
 }
